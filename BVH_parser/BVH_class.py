7aba33e541475956e8255aedbcb0d8bfd6939da3
Abdelmouttaleb Dakri <dakria@ensipc321.ensimag.fr>
2022-01-03 16:55:16 +0100

    added something

--- BVH_parser/BVH_class.py
+++ BVH_parser/BVH_class.py
@@ -3,27 +3,29 @@ import re
 
 class BvhNode:
     """
-    Class torepresent a node in the bvh file.
+    Class to represent a node in the bvh file.
     """
 
-    def __init__(self, value=[], parent=None):
-        self.value = value
-        self.children = []
-        self.parent = parent
-        if self.parent:
-            self.parent.add_child(self)
+    def __init__(self, name : str = None, offset : list = [], channels : list = [], parent=None):
+        self._name = name
+        self._offset = offset
+        self._channels = channels
+        self._children = []
+        self._parent = parent
+        if self._parent:
+            self._parent.add_child(self)
 
-    def add_child(self, item):
+    def add_child(self, item): # add another node
         item.parent = self
-        self.children.append(item)
+        self._children.append(item)
 
     def __iter__(self):
         for child in self.children:
             yield child
 
-    def __getitem__(self, key):
+    def __getitem__(self, key : str):
         for child in self.children:
-            for index, item in enumerate(child.value):
+            for names in enumerate(child._name):
                 if item == key:
                     if index + 1 >= len(child.value):
                         return None
@@ -32,25 +34,30 @@ class BvhNode:
         raise IndexError('key {} not found'.format(key))
 
     def __repr__(self):
-        return str(' '.join(self.value))
+        return "Node: " + self._name
 
 
 class HIERARCHY():
     def __init__(self, data):
-        self.data = data
-        self.root = BvhNode()
-        self.frames = []
+        self._data = data
+        self._root = BvhNode()
+        self._nodes = []
+        self._frames = []
 
     def read_listfile(self):
         first_round = []
         accumulator = ''
-        for char in self.data:
+        print(self._data)
+        for char in self._data:
+            print(char)
             if char not in ('\n', '\r'):
                 accumulator += char
+                #print(accumulator)
             elif accumulator:
                 first_round.append(re.split('\\s+', accumulator.strip()))
+                #print(first_round[-1])
                 accumulator = ''
-        node_stack = [self.root]
+        node_stack = [self._root]
         frame_time_found = False
         node = None
         for item in first_round:
@@ -69,22 +76,17 @@ class HIERARCHY():
                 frame_time_found = True
 
     def get_joints_names(self):
-        joints = []
-
-        def iterate_joints(joint):
-            joints.append(joint.value[1])
-            for child in joint.filter('JOINT'):
-                iterate_joints(child)
-        iterate_joints(next(self.root.filter('ROOT')))
+        joints = ""
+        for i in self._nodes:
+            joints += str(i._name)
         return joints
+        
 
 
 if __name__ == "__main__":
     FILE_NAME = "walk.bvh"
     bvh_file = open(FILE_NAME, 'r')
     bvh_f = HIERARCHY(list(bvh_file))
-    bvh_f.tokenize()
-    list = bvh_f.get_joints_names()
-    print(list)
+    bvh_f.read_listfile()
 
     bvh_file.close()
